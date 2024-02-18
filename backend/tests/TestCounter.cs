247e0507e82dde0234386a626bbcf8d34501aacf
Kyle Ramsy <kjramsy@gmail.com>
2024-02-18 12:52:50 -0600

    modified  the testcounter.cs

--- backend/tests/TestCounter.cs
+++ backend/tests/TestCounter.cs
@@ -17,14 +17,14 @@ namespace tests
         private readonly ILogger logger = TestFactory.CreateLogger();
 
         [Fact]
-        public async void Http_trigger_should_return_known_string()
+        public void Http_trigger_should_return_known_string()
         {
             var counter = new VisitorCounter.Function.Counter();
             counter.Id = "1";
             counter.Count = 2;
             var request = TestFactory.CreateHttpRequest();
             var response = (HttpResponseMessage) VisitorCounter.Function.GetResumeCounter.Run(request, counter, out counter, logger);
-            Assert.Equal(4, counter.Count);
+            Assert.Equal(3, counter.Count);
         }
 
     }
