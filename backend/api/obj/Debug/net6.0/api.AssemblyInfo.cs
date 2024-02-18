ac8bd3d00bf8bca1a69d47631e21b8e8181d9f44
Kyle Ramsy <kjramsy@gmail.com>
2024-02-17 21:26:40 -0600

    new attempt for workflow

--- backend/api/obj/Debug/net6.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net6.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa5efe9633fd68948bfeed82f55394c83ef5859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9000d9ad5f8f346b84651d64af8bbb37a64a96a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
